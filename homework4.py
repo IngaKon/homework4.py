51c5d96422204944577702d448975c93b9f4f542
IngaKon <kononova.inga@list.ru>
2024-06-30 14:52:52 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string= input('Введите вашу ФИО : ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace( ' ','%'))
+first=my_string[0]
+last=my_string[-1]
+print(first)
+print(last)
